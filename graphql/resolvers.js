31af790c0a7b78cee9488299804332eb61576c56
Sumin Ha <71062967+minha62@users.noreply.github.com>
2021-12-12 00:42:12 +0900

    Wrapping a REST API with GraphQL 1

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -2,8 +2,6 @@ import { getMovies, getById, addMovie, deleteMovie } from "./db";
 
 const resolvers = {
     Query: {
-        movies: (_, {rating, limit}) => getMovies(limit, rating)
+        movies: getMovies()
     }
 };
-
-export default resolvers;
